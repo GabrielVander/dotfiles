3296bd2685a1bc1ca826b98b7ad2aa4a26b1c04c
Gabriel Vander <prism1095@duckdns.com>
2026-05-10 10:54:53 -0300

    Remove unused configs

--- lazyvim_rhel/dot-config/nvim/lua/plugins/wildfire.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "gcmt/wildfire.vim",
-  event = "VeryLazy",
-  config = function()
-    -- Press ENTER to expand, BACKSPACE to shrink
-    vim.g.wildfire_objects = { "i'", 'i"', "i)", "i]", "i}", "ip", "it" }
-  end,
-}
