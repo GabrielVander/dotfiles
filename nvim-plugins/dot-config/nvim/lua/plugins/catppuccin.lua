a6d22cabf3d880fd4ec9747c18d619206ffc43e2
Gabriel Vander dos Santos Guimarães <38587412+GabrielVander@users.noreply.github.com>
2026-08-13 11:44:49 -0300

    Create catppuccin.lua

---
+++ nvim-plugins/dot-config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+-- if true then return {} end
+
+return {
+  -- add catppuccin
+  { "catppuccin/nvim", name = "catppuccin" },
+
+  -- Configure LazyVim to load catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  }
+}
