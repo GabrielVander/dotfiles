e22cb0487e9aff7c627ee3f0deb66dec0a90f04e
Gabriel Vander dos Santos Guimarães <38587412+GabrielVander@users.noreply.github.com>
2026-08-13 11:30:28 -0300

    Add mini.surround plugin configuration
    Add configuration for mini.surround plugin with key mappings.

---
+++ nvim-plugins/dot-config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,16 @@
+-- if true then return {} end
+
+return {
+  "nvim-mini/mini.surround",
+  opts = {
+    mappings = {
+      add = "gsa",
+      delete = "gsd",
+      find = "gsf",
+      find_left = "gsF",
+      highlight = "gsh",
+      replace = "gsr",
+      update_n_lines = "gsn",
+    },
+  },
+}
