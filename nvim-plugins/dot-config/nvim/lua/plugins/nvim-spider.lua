040785aa5825e8acf17ab28aed1d71ff611ec333
Gabriel Vander dos Santos Guimarães <38587412+GabrielVander@users.noreply.github.com>
2026-08-13 16:44:34 -0300

    Refactor nvim-spider configuration and key mappings

--- nvim-plugins/dot-config/nvim/lua/plugins/nvim-spider.lua
+++ nvim-plugins/dot-config/nvim/lua/plugins/nvim-spider.lua
@@ -1,20 +1,16 @@
+-- if true then return {} end
+
 return {
   "chrisgrieser/nvim-spider",
-  event = "VeryLazy",
+  lazy = true,
   opts = {
-    -- Options for the spider motion
-    subword = true,                      -- Treat subwords (camelCase, snake_case) as separate words
-    skipInsignificantPunctuation = true, -- Skip insignificant punctuation when moving
-    customPatterns = {},                 -- Add custom word patterns
+    skipInsignificantPunctuation = true,
+    subwordMovement = true,
   },
-  config = function(_, opts)
-    require("spider").setup(opts)
-
-    -- Set up key mappings
-    -- Override default word motions with spider's smarter version
-    vim.keymap.set({ "n", "x", "o" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
-    vim.keymap.set({ "n", "x", "o" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
-    vim.keymap.set({ "n", "x", "o" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
-    vim.keymap.set({ "n", "x", "o" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-ge" })
-  end,
+  keys = {
+    { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+    { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+    { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+    { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
+  },
 }
